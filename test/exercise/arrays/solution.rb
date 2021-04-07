7ef0f654da071576c47782765992489a161106f2
zlobin <zbinorama@gmail.com>
2021-04-07 20:15:05 +0300

    add Arrays.replace

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,12 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = array[0]
+        array.each do |el|
+          max = el if el > max
+        end
+
+        array.map { |el| el.abs == el ? max : el }
       end
 
       def search(_array, _query)
