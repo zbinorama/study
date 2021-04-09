42ff96d9d5408d3b68439e3cba22ac356ca230c5
zlobin <zbinorama@gmail.com>
2021-04-09 17:59:11 +0300

    add fp tasks

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,30 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+
+      def rating(array)
+        filtered_films = array.filter do |film|
+          !film['rating_kinopoisk'].nil? && !film['rating_kinopoisk'].to_f.zero? &&
+            !film['country'].nil? && film['country'].split(',').count >= 2
+        end
+
+        rating_sum = filtered_films.map do |film|
+          film['rating_kinopoisk'].to_f
+        end.reduce(:+)
+
+        rating_sum / filtered_films.count
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        char = 'и'
+
+        filtered_films = films.filter do |film|
+          !film['rating_kinopoisk'].nil? &&
+            film['rating_kinopoisk'].to_f >= threshold &&
+            film['name'].chars.include?(char)
+        end
+
+        filtered_films.map { |film| film['name'].count(char) }.reduce(:+)
       end
     end
   end
