cb264028279ce372faf9cd9b22a9d8d0fcf3fa9d
zlobin <zbinorama@gmail.com>
2021-06-15 13:43:16 +0300

    add my_map, my_compact

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -14,10 +14,16 @@ module Exercise
       end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map(&block)
+        func = ->(acc, el) { acc << block.call(el) }
+        my_reduce MyArray.new, &func
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        func = ->(acc, el) { el.nil? ? acc : acc << el }
+        my_reduce MyArray.new, &func
+      end
 
       # Написать свою функцию my_reduce
       def my_reduce(acc = nil, &block)
