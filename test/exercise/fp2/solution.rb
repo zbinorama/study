e9e4985f7fd655e7ee02c1dc1adc32ab0b9f5c41
zlobin <zbinorama@gmail.com>
2021-06-16 14:29:12 +0300

    refactoring

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,42 +5,37 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each
-        for i in 0...size
-          yield self[i]
-        end
+      def my_each(&block)
+        first, *rest = self
+
+        block.call(first)
+        MyArray.new(rest).my_each(&block) unless rest.empty?
 
         self
       end
 
       # Написать свою функцию my_map
       def my_map(&block)
-        func = ->(acc, el) { acc << block.call(el) }
+        func = ->(acc, el) { MyArray.new([*acc, block.call(el)]) }
         my_reduce MyArray.new, &func
       end
 
       # Написать свою функцию my_compact
       def my_compact
-        func = ->(acc, el) { el.nil? ? acc : acc << el }
+        func = ->(acc, el) { el.nil? ? acc : MyArray.new([*acc, el]) }
         my_reduce MyArray.new, &func
       end
 
       # Написать свою функцию my_reduce
       def my_reduce(acc = nil, &block)
-        stop_index = size - 1
-        start_index =
-          if acc.nil?
-            acc = self[0]
-            1
-          else
-            0
-          end
+        first, *rest = self
+        new_acc = acc.nil? ? first : block.call(acc, first)
 
-        for i in start_index..stop_index
-          acc = block.call(acc, self[i])
+        if rest.empty?
+          new_acc
+        else
+          MyArray.new(rest).my_reduce(new_acc, &block)
         end
-
-        acc
       end
     end
   end
